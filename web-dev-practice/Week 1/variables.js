078a47a6b0fefe6b39b7c91c77f060ebb0b51eb6
Hojikarunai120 <bradleyk120le@gmail.com>
2022-05-12 20:21:35 -0700

    first commit

---
+++ web-dev-practice/Week 1/variables.js
@@ -0,0 +1,12 @@
+var num = 5
+var Richard = "8=D";
+console.log(Richard)
+
+var sum = num - 10;
+
+console.log(sum)
+
+var First = "Bradley "
+var Last = "Le"
+var Name = First + Last 
+console.log (Name)
